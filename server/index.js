b20d3e818dc6d132fd4c62ceb0c78e5abb950fed
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-02-06 20:16:20 +0300

    Use webpackDevMiddleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const express = require('express');
+const webpackConfig = require('../webpack.config.js');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const compiler = webpack(webpackConfig);
 
 const app = express();
 const indexPath = path.join(__dirname, '/../dist/index.html');
 const staticPath = path.join(__dirname, '/../dist');
 
 app.use(express.static(staticPath));
+app.use(webpackDevMiddleware(compiler, {publicPath: webpackConfig.output.publicPath}));
+
 app.get('*', function (_, res) {
     res.sendFile(indexPath);
 })
